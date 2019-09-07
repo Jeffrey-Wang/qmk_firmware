10acb3238c45a319e224e534689ed61a2d461c35
Jeffrey-Wang <17jwapples@gmail.com>
2019-09-07 01:55:14 -0400

    update sol:jwap

---
+++ keyboards/rgbkb/sol/keymaps/jwap/rules.mk
@@ -0,0 +1,18 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+
+# RGB Options
+RGBLIGHT_ENABLE         = no        # Enable global lighting effects. Do not enable with RGB Matrix
+RGBLIGHT_ANIMATIONS     = no        # LED animations
+LED_MIRRORED            = no        # Mirror LEDs across halves (enable DIP 1 on slave, and DIP 2 and 3 on master)
+RGB_MATRIX_ENABLE       = WS2812    # Enable per-key coordinate based RGB effects. Do not enable with RGBlight
+RGB_MATRIX_KEYPRESSES   = yes
+
+ENCODER_ENABLE 			= yes
+
+# Do not edit past here
+
+include keyboards/$(KEYBOARD)/post_rules.mk
+
